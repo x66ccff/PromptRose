7ccfc1b3c2df7d8f6cf2127d167ee24897e9654d
Kai Ruan <kairuan@ruc.edu.cn>
2025-09-01 11:51:10 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 REM 激活Conda环境
+@REM call conda init
 call conda activate PromptRose
 
 REM 启动Python应用程序
@@ -8,3 +9,4 @@ python "%~dp0prompt_wheel.py"
 REM 如果需要保持窗口打开（调试用），取消下面的注释
 
 REM pause
+
